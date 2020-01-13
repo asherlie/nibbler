c5b7309fdcc9c202b004fbde402d0735b324f840
Asher Lieber <asherlie@buffalo.edu>
2020-01-12 21:35:40 -0500

    initial commit

---
+++ nibbler.c
@@ -0,0 +1,14 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int read_bytes(char* url){
+      (void)url;
+}
+
+int main(){
+      return read_bytes();
+}
