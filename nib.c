cbb103fe8abfe65c4d3b85bf516d2c2dbf0244eb
asherlie <lieber.asher@gmail.com>
2020-01-25 01:19:03 -0500

    cleaned up example code, clock_gettime() is now used to avoid weirdness with multiple threads

--- nib.c
+++ nib.c
@@ -20,64 +20,39 @@ int main(int a, char** b){
       if(a < 2)return 1;
       curl_global_init(CURL_GLOBAL_ALL);
       /*struct web_page* w = dl_pages(b+1, 1);*/
-      char* pages[] = {
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com",
-      "example.com", "example.com", "example.com", "example.com", "example.com"
-      };
-      clock_t st = clock();
+      char* pages[200] = {0};
+      char ex[] = "example.com";
+      for(int i = 0; i < 200; ++i)
+            pages[i] = ex;
+      /*
+       * use of clock() was giving inaccurate timings due to multiple threads using more
+       * cpu time at once
+      */
+      struct timespec st, fin;
+      clock_gettime(CLOCK_MONOTONIC, &st);
       int npages = 200;
       struct shash* w = dl_pages(pages, npages);
-      double el0 = ((double)clock()-st)/CLOCKS_PER_SEC;
+      clock_gettime(CLOCK_MONOTONIC, &fin);
+
+      double el0 = fin.tv_sec - st.tv_sec;
+      el0 += (fin.tv_nsec-st.tv_nsec)/1000000000.0;
+
       printf("dl and tagging took %lf\n", el0);
 
       if(a < 3)return 0;
       /*struct shash* found;*/
       /*struct shash* found = ind_shash(&h, b+2, a-2);*/
-      st = clock();
+      clock_gettime(CLOCK_MONOTONIC, &fin);
       int fail = 0, found = 0;
       for(int i = 0; i < npages; ++i){
             if(!w[i].entries)++fail;
             found += (_Bool)ind_shash(w+i, b+2, a-2);
       }
-      double el1 = ((double)clock()-st)/CLOCKS_PER_SEC;
+      clock_gettime(CLOCK_MONOTONIC, &fin);
+
+      double el1 = fin.tv_sec - st.tv_sec;
+      el1 += (fin.tv_nsec-st.tv_nsec)/1000000000.0;
+
       printf("%i/%i malformed pages\n", fail, npages);
       printf("found: %i/%i\n", found, npages);
       printf("ind_shash took %lf\nall computation took %lf seconds\n", el1, el0+el1);
