80fe7a0c7b66580f4641aff19edb9a8f55a39445
asherlie <lieber.asher@gmail.com>
2020-03-28 02:45:28 -0400

    added new function for tagging

--- tagger.c
+++ tagger.c
@@ -4,6 +4,77 @@
 
 #include "tagger.h"
 
+void taggem(struct shash* h, struct web_page* w){
+      char tag[100] = {0};
+      _Bool in_tag = 0;
+      int t_ind = 0;
+      struct shash* current = h;
+      for(unsigned int i = 0; i < w->bytes; ++i){
+            char c = w->data[i];
+            /* start tag */
+            if(c == '<'){
+                  in_tag = 1;
+                  t_ind = 0;
+                  memset(tag, 0, 100);
+            }
+            else if(c == '>'){
+                  if(in_tag){
+                        in_tag = 0;
+                        if(tag[t_ind-1] == '/')continue;
+
+                        /* current must be set to parent shash */
+                        if(tag[0] == '/'){
+                              current = current->parent;
+                              continue;
+                        }
+
+                        /* inserting a tag into current->entries */
+                        printf("tag: \"%21s\" @ %p\n", tag, (void*)current);
+                        /*if(!current)puts("ANTICIPATED");*/
+
+                        int bucket = (tag[1])%current->nbux;
+                        /* if bucket doesn't exist, create it */
+                        if(!current->entries[bucket]){
+                              current->entries[bucket] = calloc(1, sizeof(struct sh_entry));
+                              memcpy(current->entries[bucket]->tag, tag, t_ind);
+                              init_shash(current->entries[bucket]->subhash = malloc(sizeof(struct shash)));
+                              current->entries[bucket]->last = current->entries[bucket];
+                              current->entries[bucket]->last->next = NULL;
+                              /*current->last = current->entries[bucket];*/
+                              /*current->entries[bucket]->subhash->last = ;*/
+                              /*current->entries[bucket]->subhash->*/
+                        }
+                        /* otherwise, insert in back of LL */
+                        else{
+                              /* abstract this */
+                              current->entries[bucket]->last->next = calloc(1, sizeof(struct sh_entry));
+                              current->entries[bucket]->last = current->entries[bucket]->last->next;
+                              /*current->entries[bucket]->last->next = NULL;*/
+                              memcpy(current->entries[bucket]->last->tag, tag, t_ind);
+                              init_shash(current->entries[bucket]->last->subhash = malloc(sizeof(struct shash)));
+
+                        }
+
+                        /* updating current */
+                        /*
+                         * current->entries[bucket]->subhash->parent = current;
+                         * current = current->entries[bucket]->subhash;
+                        */
+
+                        current->entries[bucket]->last->subhash->parent = current;
+                        current = current->entries[bucket]->last->subhash;
+
+                        /*printf("bucket %i\n", bucket);*/
+                        /*current->entries[];*/
+                        /* tag end */
+                  }
+            }
+            else if(in_tag){
+                  tag[t_ind++] = c;
+            }
+      }
+}
+
 _Bool tag_page(struct shash* h, struct web_page* w){
       char* raw_page = w->data;
       size_t strlen = w->bytes;
